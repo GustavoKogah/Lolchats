dff7ce9678ef622eb9f83f392bb12e2e969274f1
Gustavo Kogah <gustavokogah@gmail.com>
2022-09-29 09:52:54 -0300

    correcao constraint

--- fastLearning/ViewController.swift
+++ fastLearning/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class Conversa {
+    let nomeDaPessoa : String
+    let ultimaMensagem : String
+    
+    init(nomeDaPessoa: String, ultimaMensagem: String){
+        self.nomeDaPessoa = nomeDaPessoa
+        self.ultimaMensagem = ultimaMensagem
+    }
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var conversas: [Conversa] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        
+        let conversa1 = Conversa(nomeDaPessoa: "Kogah", ultimaMensagem: "Testando o App")
+        let conversa2 = Conversa(nomeDaPessoa: "Helio", ultimaMensagem: "Vai dar bom")
+        let conversa3 = Conversa(nomeDaPessoa: "Zampo", ultimaMensagem: "Deu pau o PJ")
+        let conversa4 = Conversa(nomeDaPessoa: "Vivi", ultimaMensagem: "Bom dia time")
+        let conversa5 = Conversa(nomeDaPessoa: "Natan", ultimaMensagem: "Não entendi")
+        let conversa6 = Conversa(nomeDaPessoa: "Fernandão", ultimaMensagem: "Bora fazer")
+        let conversa7 = Conversa(nomeDaPessoa: "Timem Implmentação", ultimaMensagem: "Não vai dar pra subir")
+        let conversa8 = Conversa(nomeDaPessoa: "GSI", ultimaMensagem: "Temos um incidente para vocês")
+        let conversa9 = Conversa(nomeDaPessoa: "Caio", ultimaMensagem: "E o Firebase? ")
+        
+        conversas.append(conversa1)
+        conversas.append(conversa2)
+        conversas.append(conversa3)
+        conversas.append(conversa4)
+        conversas.append(conversa5)
+        conversas.append(conversa6)
+        conversas.append(conversa7)
+        conversas.append(conversa8)
+        conversas.append(conversa9)
+    
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return conversas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "whatsappCell", for: indexPath) as! WhatsAppCellTableViewCell
+        cell.titleLabel.text = conversas[indexPath.row].nomeDaPessoa
+        cell.subtitleLabel.text = conversas[indexPath.row].ultimaMensagem
+        return cell
+    }
 
 }
 
---
+++ fastLearning/WhatsAppCellTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  WhatsAppCellTableViewCell.swift
+//  fastLearning
+//
+//  Created by Luis Gustavo Fermino Moraes on 28/09/22.
+//
+
+import UIKit
+
+class WhatsAppCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bulletView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        profileImage.layer.cornerRadius = 25
+        profileImage.clipsToBounds = true
+        bulletView.layer.cornerRadius = 12
+        bulletView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
